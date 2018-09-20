839bbc9c881ed7c4bcdd1f7b17e3fde3f761920a
Albert J. Wong <awong-dev@gmail.com>
2018-09-19 23:52:39 -0700

    All works! I think.

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App(props) {
+  return (
+    <div>
+     Hi everyone. How are you?
+    </div>
+  );
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
-const glob = require('glob');
 const webpack = require('webpack');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const extractSass = new ExtractTextPlugin({
-      filename: "[name].css"
+const extractSass = new MiniCssExtractPlugin({
+  filename: "[name].css",
+  chunkFilename: "[id].css"
 });
 
 const config = {
-  entry: { lights: ["./client/lights-entry.jsx"] },
+  entry: { main: ["./client/main-entry.jsx"] },
   resolve: { extensions: ['.js', '.jsx'] },
   output: {
     path: path.join(__dirname, `./public/generated`),
@@ -21,34 +21,25 @@ const config = {
     rules: [
       {
         test: /\.scss$/,
-        use: extractSass.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader' },
-            { loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-                includePaths: glob.sync('node_modules').map((d) => path.join(__dirname, d)),
-              } }
-          ],
-          fallback: 'style-loader'
-        })
-      },
-	 {
-	   test: /\.jsx?$/,
+        use: [
+           MiniCssExtractPlugin.loader,
+           { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+           { loader: 'sass-loader', options: { sourceMap: true } },
+         ],
+      },{
+        test: /\.jsx?$/,
         include: [
           path.resolve(__dirname, 'client'),
           path.resolve(__dirname, 'node_modules/@material')
         ],
-	   use: {
-		loader: 'babel-loader',
-		options: {
-		  // Also see .babelrc
-		  cacheDirectory: '.babelcache',
-            presets: ['env']
-		}
-	   }
-	 }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Also see .babelrc
+            cacheDirectory: '.babelcache',
+          }
+        }
+      }
     ],
   },
   plugins: [
@@ -64,9 +55,10 @@ const config = {
 
 if (isDevelopment) {
   config.devtool = '#eval-source-map';
+  config.mode = 'development';
 } else {
   config.devtool = '#source-map';
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: true}));
+  config.mode = 'production';
 }
 
 module.exports = config;
